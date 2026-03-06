d521394fc7e2e22201974306e6d8e368cb1437d4
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2026-03-06 16:17:04 +0530

    Here the information about variables and Data Types

---
+++ basic/variables.py
@@ -0,0 +1,26 @@
+a = 1235469 #Here a is a Variable
+print(a)
+
+b = "Code"
+print(b)
+
+#print(a + b) # here a error occured because a is a number and b is a string
+
+a1 = 9
+print(a + a1)
+
+print("The type of a is: ",type(a)) #to know the data type of variable 'a'
+print("The type of a is: ",type(b)) 
+print("The type of a is: ",type(a1)) 
+
+
+
+#..............DataTypes............
+A = 4
+B= True
+C = "Anna"
+D = None
+print("The type of a is: ",type(A)) 
+print("The type of a is: ",type(B)) 
+print("The type of a is: ",type(C)) 
+print("The type of a is: ",type(D)) 
