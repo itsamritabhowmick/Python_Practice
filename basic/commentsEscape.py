3751faed5d8bd87ba4a4815bd052cae0bccb4253
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2026-03-06 13:46:18 +0530

    Here the information about Comments, Escape sequences

---
+++ basic/commentsEscape.py
@@ -0,0 +1,27 @@
+# For a single line comment you use only one "#"
+
+'''
+for multiple line comments you can use this structure 
+'''
+
+
+
+
+print("Hey I am a girl \nand this viewer is also a good human being")
+# here "\n" use for enter a new line...it is also called Escape sequence
+
+
+print ("I like to eat icecream \"and also\"\nI like to eat Rasgulla")
+#This is called double-quote
+
+
+print("Hey",6,7)
+# when we want to print multiple values
+
+
+print("Potato",2,9, sep="~") # sep means Separator
+# If you ever want to join any 3 words so you can do this 
+
+
+print("Hey",6,7, sep="~", end = "009") # here end is used to print when the print statement will end
+print("You")
