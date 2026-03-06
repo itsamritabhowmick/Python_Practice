c4d92fee95ae96356b77836dd5263f2f0646f39b
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2026-03-06 16:27:25 +0530

    Here is the one small exercise practice

---
+++ basic/calculator.py
@@ -0,0 +1,7 @@
+a = 30
+b = 2
+
+print("The value of",a, "+" ,b," is:",a+b)
+print("The value of",a, "-" ,b," is:",a-b)
+print("The value of",a, "*" ,b," is:",a*b)
+print("The value of",a, "/" ,b," is:",a/b)
